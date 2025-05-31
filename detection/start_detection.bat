3c91287389ab497ef5323300a885820f89dae710
MM Salman Faris <110341513+mmsalmanfaris@users.noreply.github.com>
2025-05-31 14:27:03 +0530

    Delete detection directory

--- detection/start_detection.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo -------------------------------------
-echo Activating Python 3.10 Virtual Env
-echo -------------------------------------
-cd /d %~dp0
-call venv310\Scripts\activate.bat
